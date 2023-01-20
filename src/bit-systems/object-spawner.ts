a8e0376b05d8ddd6a3d81c4b0cc5d58e4c1655e7
John Shaughnessy <john@johnshaughnessy.com>
2023-01-19 21:56:46 -0500

    Give InitialData a (non-any) type

--- src/bit-systems/object-spawner.ts
+++ src/bit-systems/object-spawner.ts
@@ -22,7 +22,7 @@ function* waitForMediaLoaded(world: HubsWorld, eid: EntityID) {
 
 function* spawnObjectJob(world: HubsWorld, spawner: EntityID) {
   const spawned = createNetworkedEntity(world, "media", {
-    src: APP.getString(ObjectSpawner.src[spawner]),
+    src: APP.getString(ObjectSpawner.src[spawner])!,
     recenter: false,
     resize: false,
     animateLoad: false,
